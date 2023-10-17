1ee317310a666d599f32781634ee33c14b5388b7
eltanshark <62362561+eltanshark@users.noreply.github.com>
2023-10-17 21:15:07 +0700

    Updating prediction logic

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,15 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const kprForm = document.getElementById('kprForm');
-    const calculateButton = document.getElementById('calculateButton');
+function updateMaritalStatus() {
+    // Get element form Married_Yes
+    var marriedYesForm = document.getElementById('Married_Yes');
+    // Get element form hidden Married_No
+    var marriedNoForm = document.getElementById('Married_No');
 
-    calculateButton.addEventListener('click', function() {
-        const creditHistory = document.getElementById('creditHistory').value;
-        const gender = document.getElementById('gender').value;
-        const married = document.getElementById('married').value;
+    // Memperbarui nilai pada Married_No berdasarkan nilai Married_Yes
+    // Update value on `Married_No` depends on `Married_Yes` value
+    if (marriedYesForm.value === '1') {
+    marriedNoForm.value = '0';
+    } else if (marriedYesForm.value === '0'){
+    marriedNoForm.value ='1';
+    }   else
+    marriedNoForm.value = '';  // null if != 1 || 0
+    }
 
-        // Di sini Anda dapat menambahkan logika perhitungan KPR berdasarkan input yang diberikan.
-        // Contoh:
-        const hasilPerhitungan = `Anda memasukkan Credit History: ${creditHistory}, Gender: ${gender}, Married: ${married}`;
-        alert(hasilPerhitungan);
-    });
-});
+function updateGender(){
+    var genderMaleForm = document.getElementById('Gender_Male');
+    var genderFemaleForm = document.getElementById('Gender_Female');
+
+    if (genderMaleForm.value === '1'){
+        genderFemaleForm.value = '0';
+    } else if (genderMaleForm.value === '0'){
+        genderFemaleForm.value = '1';
+    } else 
+    genderMaleForm.value ='';
+}
